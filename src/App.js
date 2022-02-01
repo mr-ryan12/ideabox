5abf41a390b7c515e538b88f2a933cef58a35e60
Ryan McBride <mr.ryan444@gmail.com>
2022-01-31 17:20:42 -0700

    Add ideas component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Ideas from './components/Ideas'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ideas: [
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <h1 className="App">IdeaBox</h1>
+        <Ideas />
+      </main>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Ideas.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import '../Ideas.css'
+
+const Ideas = props => {
+  return (
+    <h2>Ideas go here!</h2>
+  )
+}
+
+export default Ideas
